45c18374113eb0283e41470612456cba5a931a29
tommykalmer <tommy.kalmer@gmail.com>
2025-03-25 23:58:47 +0200

    Första kod in i filen

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from backend.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    license = Column(String)  # gratis, premium, guld
 
