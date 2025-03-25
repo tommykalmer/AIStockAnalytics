8e7b0da19845046f1fa8a81f66b05494f4a81c0c
tommykalmer <tommy.kalmer@gmail.com>
2025-03-25 23:58:07 +0200

    Första kod in i filen

--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Skapa databasfilen SQLite lokalt
+DATABASE_URL = "sqlite:///./aistockanalytics.db"
+
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+# Skapa en session som används av alla db-anrop
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Bas för alla modeller
+Base = declarative_base()
 
