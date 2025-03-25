43718670c43ca7956680d47d7649603bfa088bc8
tommykalmer <tommy.kalmer@gmail.com>
2025-03-25 23:57:29 +0200

    Första kod in i filen

--- backend/init_db.py
+++ backend/init_db.py
@@ -1,1 +1,6 @@
+from backend.database import Base, engine
+from backend import models
 
+print("Skapar databasen och tabellerna...")
+Base.metadata.create_all(bind=engine)
+print("✅ Databas klar! Tabellen 'users' är skapad.")
